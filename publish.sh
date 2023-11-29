f4f12afa446b131d89f35c95df3eb599e16d9c67
Antonius Naumann <git@antonius.dev>
2023-11-29 22:57:09 +0100

    Update license and add simple script for publishing all workspace crates

---
+++ publish.sh
@@ -0,0 +1,21 @@
+cd galvan-files
+cargo publish
+cd ..
+
+cd galvan-test-macro
+cargo publish
+cd ..
+
+cd galvan-pest
+cargo publish
+cd ..
+
+cd galvan-ast
+cargo publish
+cd ..
+
+cd galvan-transpiler
+cargo publish
+cd ..
+
+cargo publish
