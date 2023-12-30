761caf8cb4aa7cdda2f657ba8be4242676148b8d
Antonius Naumann <git@antonius.dev>
2023-12-30 08:22:23 +0100

    Update macro for adding transpiler mappings for builtin types

--- galvan-transpiler/src/builtins.rs
+++ galvan-transpiler/src/builtins.rs
@@ -3,27 +3,27 @@ use crate::mapping::{mapping, Mapping};
 /// Mapping of standard Galvan types to Rust types
 pub fn builtins() -> Mapping {
     mapping!(
-        "Bool" => "bool", "&bool", "&mut bool",
+        ("Bool" => "bool", copy),
 
-        "I8" => "i8", "i8", "&mut i8",
-        "I16" => "i16", "i16", "&mut i16",
-        "I32" => "i32", "i32", "&mut i32",
-        "I64" => "i64", "i64", "&mut i64",
-        "I128" => "i128", "&i128", "&mut i128",
-        "Int" => "i64", "i64", "&mut i64",
-        "ISize" => "isize", "isize", "&mut isize",
+        ("I8" => "i8", copy),
+        ("I16" => "i16", copy),
+        ("I32" => "i32", copy),
+        ("I64" => "i64", copy),
+        ("I128" => "i128", copy),
+        ("Int" => "i64", copy),
+        ("ISize" => "isize", copy),
 
-        "U8" => "u8", "u8", "&mut u8",
-        "U16" => "u16", "u16", "&mut u16",
-        "U32" => "u32", "u32", "&mut u32",
-        "U64" => "u64", "u64", "&mut u64",
-        "U128" => "u128", "u128", "&mut u128",
-        "UInt" => "u64", "u64", "&mut u64",
-        "USize" => "usize", "usize", "&mut usize",
+        ("U8" => "u8", copy),
+        ("U16" => "u16", copy),
+        ("U32" => "u32", copy),
+        ("U64" => "u64", copy),
+        ("U128" => "u128", copy),
+        ("UInt" => "u64", copy),
+        ("USize" => "usize", copy),
 
-        "Float" => "f32", "f32", "&mut f32",
-        "Double" => "f64", "f64", "&mut f64",
+        ("Float" => "f32", copy),
+        ("Double" => "f64", copy),
 
-        "String" => "String", "&str", "&mut String",
+        ("String" => "String", "str"),
     )
 }
--- galvan-transpiler/src/mapping.rs
+++ galvan-transpiler/src/mapping.rs
@@ -31,19 +31,63 @@ impl Mapping {
             .map(Cow::Borrowed)
             .unwrap_or_else(|| format!("&mut {}", type_id.as_str()).into())
     }
+
+    pub(crate) fn is_copy(&self, type_id: &TypeIdent) -> bool {
+        self.types
+            .get(type_id)
+            .map(|rust_type| rust_type.is_copy)
+            .unwrap_or(false)
+    }
 }
 
 macro_rules! mapping {
-    ($($type_id:literal => $owned:literal, $borrowed:literal, $mut_borrowed:literal),* $(,)?) => {
+    ($($tokens:tt),* $(,)?) => {
         {
             use crate::mapping::{Mapping, RustType};
-            let mut types = ::std::collections::HashMap::new();
-            $(types.insert(::galvan_ast::TypeIdent::new($type_id), RustType::new($owned, $borrowed, $mut_borrowed));)*
-            Mapping { types }
+            let types = ::std::collections::HashMap::new();
+            let mut mapping = Mapping { types };
+            $(
+                crate::mapping::mapping_insert!(mapping, $tokens);
+            )*
+            mapping
         }
     };
 }
+macro_rules! mapping_insert {
+    ($mapping:ident, ($type_id:literal => $owned:literal, $borrowed:literal, $mut_borrowed:literal, copy)) => {
+        $mapping.types.insert(
+            ::galvan_ast::TypeIdent::new($type_id),
+            RustType::new($owned, $borrowed, $mut_borrowed, true),
+        );
+    };
+    ($mapping:ident, ($type_id:literal => $owned:literal, $borrowed:literal, $mut_borrowed:literal)) => {
+        $mapping.types.insert(
+            ::galvan_ast::TypeIdent::new($type_id),
+            RustType::new($owned, $borrowed, $mut_borrowed, false),
+        );
+    };
+    ($mapping:ident, ($type_id:literal => $owned:literal, $borrowed:literal)) => {
+        $mapping.types.insert(
+            ::galvan_ast::TypeIdent::new($type_id),
+            RustType::new($owned, $borrowed, $owned, false),
+        );
+    };
+    ($mapping:ident, ($type_id:literal => $owned:literal)) => {
+        $mapping.types.insert(
+            ::galvan_ast::TypeIdent::new($type_id),
+            RustType::new($owned, $owned, $owned, false),
+        );
+    };
+    ($mapping:ident, ($type_id:literal => $owned:literal, copy)) => {
+        $mapping.types.insert(
+            ::galvan_ast::TypeIdent::new($type_id),
+            RustType::new($owned, $owned, $owned, true),
+        );
+    };
+}
+
 pub(crate) use mapping;
+pub(crate) use mapping_insert;
 
 /// Transpiled type names, depending on whether they are owned, borrowed, or mutably borrowed.
 #[derive(Debug)]
@@ -51,6 +95,7 @@ pub(crate) struct RustType {
     owned: Box<str>,
     borrowed: Box<str>,
     mut_borrowed: Box<str>,
+    is_copy: bool,
 }
 
 impl RustType {
@@ -58,11 +103,13 @@ impl RustType {
         owned: impl Into<Box<str>>,
         borrowed: impl Into<Box<str>>,
         mut_borrowed: impl Into<Box<str>>,
+        is_copy: bool,
     ) -> Self {
         Self {
             owned: owned.into(),
             borrowed: borrowed.into(),
             mut_borrowed: mut_borrowed.into(),
+            is_copy,
         }
     }
 
