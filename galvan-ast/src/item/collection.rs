34bf0015f99f7e9a7918c36b38e155e338bda70d
Antonius Naumann <git@antonius.dev>
2024-02-27 00:00:24 +0100

    Remove pest conversion from collection literal ast nodes

--- galvan-ast/src/item/collection.rs
+++ galvan-ast/src/item/collection.rs
@@ -1,39 +1,32 @@
 use crate::Expression;
-use galvan_pest::Rule;
 use typeunion::type_union;
 
 #[type_union]
-#[derive(Debug, PartialEq, Eq, FromPest)]
-#[pest_ast(rule(Rule::collection_literal))]
+#[derive(Debug, PartialEq, Eq)]
 pub type CollectionLiteral = ArrayLiteral + DictLiteral + SetLiteral + OrderedDictLiteral;
 
-#[derive(Debug, PartialEq, Eq, FromPest)]
-#[pest_ast(rule(Rule::array_literal))]
+#[derive(Debug, PartialEq, Eq)]
 pub struct ArrayLiteral {
     pub elements: Vec<Expression>,
 }
 
-#[derive(Debug, PartialEq, Eq, FromPest)]
-#[pest_ast(rule(Rule::dict_literal))]
+#[derive(Debug, PartialEq, Eq)]
 pub struct DictLiteral {
     pub elements: Vec<DictLiteralElement>,
 }
 
-#[derive(Debug, PartialEq, Eq, FromPest)]
-#[pest_ast(rule(Rule::dict_literal_element))]
+#[derive(Debug, PartialEq, Eq)]
 pub struct DictLiteralElement {
     pub key: Expression,
     pub value: Expression,
 }
 
-#[derive(Debug, PartialEq, Eq, FromPest)]
-#[pest_ast(rule(Rule::set_literal))]
+#[derive(Debug, PartialEq, Eq)]
 pub struct SetLiteral {
     pub elements: Vec<Expression>,
 }
 
-#[derive(Debug, PartialEq, Eq, FromPest)]
-#[pest_ast(rule(Rule::ordered_dict_literal))]
+#[derive(Debug, PartialEq, Eq)]
 pub struct OrderedDictLiteral {
     pub elements: Vec<DictLiteralElement>,
 }
