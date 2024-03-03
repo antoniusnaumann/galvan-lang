e588e73929066cd4ad41dbd4ce0892d51af7b170
Antonius Naumann <git@antonius.dev>
2024-03-04 00:38:22 +0100

    Print parse tree when running parser executable

--- galvan-parse/src/lib.rs
+++ galvan-parse/src/lib.rs
@@ -2,16 +2,29 @@
 pub mod exec;
 
 use galvan_files::Source;
+use thiserror::Error;
 use tree_sitter::{Parser, Tree};
 
-pub type ParseResult = Result<ParseTree, ParseError>;
-pub type ParseError = ();
+#[derive(Debug, Error)]
+#[error("Could not parse source: {path}")]
+pub struct ParseError { path: Box<str> }
 pub type ParseTree = Tree;
+pub type ParseResult = Result<ParseTree, ParseError>;
 
 pub fn parse_source(source: &Source) -> ParseResult {
     let mut parser = Parser::new();
-    parser.set_language(tree_sitter_galvan::language()).expect("Error loading Galvan grammar!");
+    parser
+        .set_language(tree_sitter_galvan::language())
+        .expect("Error loading Galvan grammar!");
 
     let content = source.content();
-    parser.parse(content, None).ok_or(())
+    parser.parse(content, None).ok_or_else(|| { 
+        let file = match source {
+            Source::File { path, content: _, canonical_name: _ } => path.to_string_lossy(),
+            Source::Str(_) => "{input string}".into(),
+            Source::Missing => "{missing}".into(),
+            Source::Builtin => "{builtin}".into(),
+
+        };
+        ParseError { path: file.into() } })
 }
--- galvan-parse/src/main.rs
+++ galvan-parse/src/main.rs
@@ -1,8 +1,11 @@
 use galvan_parse::exec::parse_current_dir;
 use std::path::Path;
+use tree_sitter::{Node, Tree};
 
 fn main() {
+    println!("Parsing...");
     let src = parse_current_dir();
+    println!("Parsing finished!");
     for (parsed, source) in &src {
         println!();
         println!(
@@ -13,7 +16,7 @@ fn main() {
                 .unwrap_or("?".into())
         );
         match parsed {
-            Ok(p) => println!("Parsed: {:#?}", p),
+            Ok(p) => print_tree(p),
             Err(e) => println!("Error: {}", e),
         }
     }
@@ -24,4 +27,21 @@ fn main() {
         src.iter().filter(|(p, _)| p.is_ok()).count(),
         src.len()
     );
+}
+
+fn print_tree(t: &Tree) {
+    println!("Parsed:");
+    print_node(t.root_node(), 0);
+}
+
+fn print_node(n: Node, indent: usize) {
+    let pre = "  ".repeat(indent);
+    println!("{pre}{}", n.kind());
+
+    let mut cursor = n.walk();
+    for child in n.children(&mut cursor) {
+        if child.is_named() {
+            print_node(child, indent + 1);
+        }
+    }
 }
