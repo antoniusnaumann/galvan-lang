9f666f94f59b3d30ba1133e0da4fdfc01e5f120e
Antonius Naumann <git@antonius.dev>
2024-05-18 02:36:15 +0200

    Parse type literals

--- galvan-into-ast/src/items/type_element.rs
+++ galvan-into-ast/src/items/type_element.rs
@@ -1,7 +1,7 @@
 use galvan_ast::{ArrayTypeItem, BasicTypeItem, DictionaryTypeItem, GenericTypeItem, OptionalTypeItem, OrderedDictionaryTypeItem, ResultTypeItem, SetTypeItem, Span, TupleTypeItem, TypeElement, TypeIdent};
 use galvan_parse::TreeCursor;
 
-use crate::{cursor_expect, result::CursorUtil, AstError, ReadCursor};
+use crate::{cursor_expect, result::CursorUtil, AstError, ReadCursor, SpanExt};
 
 impl ReadCursor for TypeElement {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
@@ -21,11 +21,11 @@ impl ReadCursor for TypeElement {
             "generic_type" => TypeElement::Generic(GenericTypeItem::read_cursor(cursor, source)),
             "basic_type" => TypeElement::Plain(BasicTypeItem::read_cursor(cursor, source)),
             unknown => unimplemented!("Encountered type element not known to AST converstion: {unknown}"),
-        }
+        };
 
         // TODO  Verify that there is no other child node
 
-        cursor.goto_parent()
+        cursor.goto_parent();
 
         Ok(inner)
     }
@@ -39,31 +39,110 @@ impl ReadCursor for ResultTypeItem {
 
 impl ReadCursor for OptionalTypeItem {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
-        todo!()
+        let optional = cursor_expect!(cursor, "optional_type");
+        let span = Span::from_node(optional);
+
+        cursor.goto_first_child();
+        let some = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _question_mark = cursor_expect!(cursor, "question_mark");
+
+        cursor.goto_parent();
+
+        Self { some, span }.into()
     }
 }
 
 impl ReadCursor for ArrayTypeItem {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
-        todo!()
+        let array = cursor_expect!(cursor, "array_type");
+        let span = Span::from(array);
+
+        cursor.goto_first_child();
+        let _bracket = cursor_expect!(cursor, "bracket_open");
+
+        cursor.goto_next_sibling();
+        let inner = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _bracket = cursor_expect!(cursor, "bracket_close");
+
+        cursor.goto_parent();
+
+        Self { elements: inner, span }.into()
     }
 }
 
 impl ReadCursor for DictionaryTypeItem {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
-        todo!()
+        let dict = cursor_expect!(cursor, "dict_type");
+        let span = Span::from(dict);
+
+        cursor.goto_first_child();
+        let _brace = cursor_expect!(cursor, "brace_open");
+        
+        cursor.goto_next_sibling();
+        let key = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _colon = cursor_expect!(cursor, "colon");
+
+        cursor.goto_next_sibling();
+        let value = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _brace = cursor_expect!(cursor, "brace_close");
+
+        cursor.goto_parent();
+
+        Self { key, value, span }.into()
     }
 }
 
 impl ReadCursor for OrderedDictionaryTypeItem {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
-        todo!()
+        let dict = cursor_expect!(cursor, "ordered_dict_type");
+        let span = Span::from(dict);
+
+        cursor.goto_first_child();
+        let _bracket = cursor_expect!(cursor, "bracket_open");
+        
+        cursor.goto_next_sibling();
+        let key = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _colon = cursor_expect!(cursor, "colon");
+
+        cursor.goto_next_sibling();
+        let value = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _bracket = cursor_expect!(cursor, "bracket_close");
+
+        cursor.goto_parent();
+
+        Self { key, value, span }.into()
     }
 }
 
 impl ReadCursor for SetTypeItem {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
-        todo!()
+        let set = cursor_expect!(cursor, "set_type");
+        let span = Span::from(set);
+
+        cursor.goto_first_child();
+        let _brace = cursor_expect!(cursor, "brace_close");
+
+        cursor.goto_next_sibling();
+        let inner = TypeElement::read_cursor(cursor, source);
+
+        cursor.goto_next_sibling();
+        let _brace = cursor_expect!(cursor, "brace_close");
+
+        cursor.goto_parent();
+
+        Self { elements: inner, span }.into()
     }
 }
 
@@ -90,15 +169,15 @@ impl ReadCursor for BasicTypeItem {
 
         cursor.goto_parent();
 
-        Ok(BasicTypeItem { ident, span })
+        Self { ident, span }.into()
     }
 }
 
 impl ReadCursor for TypeIdent {
     fn read_cursor(cursor: &mut TreeCursor<'_>, source: &str) -> Result<Self, AstError> {
         let ident = cursor_expect!(cursor, "type_ident");
-        let inner = 
+        let inner = source[ident.start_byte()..=ident.end_byte()];
 
-        Ok(TypeIdent::new(inner))
+        Self::new(inner).into()
     }
 }
